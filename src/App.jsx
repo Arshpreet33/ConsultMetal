bc0ac35dc2f5a258a43a110302e6a37f9dafed0f
arshpreet33 <arshpreetsingh33@gmail.com>
2025-12-28 19:40:10 -0700

    fixed issues in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import NewContactPage from './pages/new/ContactPage';
 
 // Old Design Layout & Pages
 import OldLayout from './components/Layout/OldLayout';
-import OldAboutPage from './pages/AboutPage';
+// import OldAboutPage from './pages/AboutPage';
 // import OldAboutPageV2 from './pages/AboutPageV2';
 import OldContactPage from './pages/ContactPage';
 import OldProductsPage from './pages/ProductsPage';
@@ -66,7 +66,7 @@ function App() {
           {/* Old Design Routes */}
           <Route path="/old" element={<OldLayout />}>
             <Route index element={<OldAboutPage />} />
-            <Route path="about" element={<OldAboutPage />} />
+            {/* <Route path="about" element={<OldAboutPage />} /> */}
             {/* <Route path="about-v2" element={<OldAboutPageV2 />} /> */}
             <Route path="contact" element={<OldContactPage />} />
             <Route path="products" element={<OldProductsPage />} />
